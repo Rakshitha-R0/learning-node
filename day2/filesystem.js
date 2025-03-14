e1a867a27a02412de657b8cec100dc772f9bc6e4
Rakshitha <rakshitharaj748@gmail.com>
2025-03-14 14:00:36 +0530

    update day 2

---
+++ day2/filesystem.js
@@ -0,0 +1,17 @@
+// utilities to deal with file system
+
+import fs from 'fs';
+
+// fs.writeFileSync('./demo.txt','good afternoon');
+
+// let data = fs.readFileSync('./demo.txt','utf-8');
+
+// console.log(data);
+// console.log(data.toString('utf-8'));
+
+// fs.appendFileSync('./demo.txt','\ngood afternoon once again');
+
+// fs.unlinkSync('./demo.txt')
+
+
+// fs.mkdirSync('demo.txt', { recursive: true },);
